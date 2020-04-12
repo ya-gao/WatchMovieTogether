cbc1a98b38ab1c963fe160be47913be896fe5380
Zheming Gu <zheminggugu@gmail.com>
2020-04-11 23:08:43 -0400

    invite gorup member
    invite gorup member

--- MyProject/invitations/api.py
+++ MyProject/invitations/api.py
@@ -1,6 +1,9 @@
 from .models import Invitation
 from rest_framework import viewsets, permissions, generics
 from .serializers import InvitationSerializer
+from rest_framework.response import Response
+from wmt.models import GroupExtend 
+from django.contrib.auth.models import User
 
 # viewset basically allows us to create a full CRUD API without having to specigy explicit methods for the functionality
 
@@ -21,3 +24,16 @@ class InvitationViewSet(viewsets.ModelViewSet):
     # save the inviter when send invitation
     def perform_create(self, serializer):
         serializer.save(inviter_username=self.request.user.username)
+
+    def destroy(self, request, *args, **kwargs):
+        print("perform destroy")
+        instance = self.get_object()
+        print(instance)
+        group = GroupExtend.objects.get(id=instance.group_id)
+        print(group)
+        new_user = User.objects.get(username=instance.invited_username) 
+        group.members.add(new_user)
+        group.save()
+        self.perform_destroy(instance)
+        return Response()
+
